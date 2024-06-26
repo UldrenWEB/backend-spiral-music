a965394415b62366dd37b94ebb0b34590c0a2976
eliohernanco@gmail.com <eliohernanco@gmail.com>
2024-06-26 00:27:30 -0400

    fix

--- src/routes/songs.js
+++ src/routes/songs.js
@@ -2,6 +2,10 @@ import express from "express";
 const router = express.Router()
 import SongsController from '../controllers/Songs/getSongController.js';
 import SongsByArtistController from '../controllers/Songs/getSongByArtistController.js';
+import getSongById from '../controllers/Songs/getSongByIdController.js';
+import SongsByGenresController  from '../controllers/Songs/getSongByGenresController.js';
+
+const songsControllerInstanceByGenres = new SongsByGenresController();
 
 const songsControllerInstanceByArtist = new SongsByArtistController();
 
@@ -16,10 +20,19 @@ router.get('/get-songs/:name/:offset?', (req, res ) => {
     songsControllerInstance.getSongsbyName(req, res);
   });
   
-  export default router;
 
 //Middleware para instanciar el controlador y llamar al metodo correcto para getSongsByArtist  
 router.get('/get-songs-by-artist/:name/:offset?', (req, res ) => {
     const songsControllerInstanceByArtist = new SongsByArtistController();
     songsControllerInstanceByArtist.getSongsByArtist(req, res);
   });
+
+   // Obtener canciones por Id
+   router.get('/get-song/:id', getSongById);
+
+   // Obtener canciones por generos
+   router.get('/get-songs-by-genres/', songsControllerInstanceByGenres.getSongsByGenres);
+
+export default router
+
+
